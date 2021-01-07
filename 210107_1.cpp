6f250eed30cb6913f4508f5c7424085030fbc063
mini <guslovealsgm@dongguk.edu>
2021-01-07 10:50:32 +0900

    Second commit

--- 210107_1.cpp
+++ 210107_1.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
+// #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    // cout << "Hello World!" << endl;
+    printf("Hello World\n");
 
     return 0;
 }
