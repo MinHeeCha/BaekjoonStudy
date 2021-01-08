22845c67e2dee8b8350b3df46a984ae843d948a6
mini <guslovealsgm@dongguk.edu>
2021-01-08 16:42:16 +0900

    Third commit"

--- 210108_1236.cpp
+++ 210108_1236.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int main() {
     int n, m;       // 세로 가로
+    int r = 0, c = 0;
+
     cin >> n >> m;
     
     if (n > 50 || m > 50) {
@@ -12,11 +14,30 @@ int main() {
         return 0;
     }
 
-    vector<string> rows(n);
+    vector<bool> rows(n);
+    vector<bool> cols(m);
 
-    for (int i = 0; i < rows.size(); i++) {
-        cin >> rows[i];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            char state;
+            cin >> state;
+
+            if (state == 'X') {
+                rows[i] = true;
+                cols[j] = true;
+            }
+        }
     }
+
+    for (int i = 0; i < n; i++)
+        if (!rows[i])
+            r++;
+    for (int i = 0; i < m; i++)
+        if (!cols[i])
+            c++;
+
+    int result = r < c ? c : r;
+    cout << result << endl;
 
     return 0;
 }
