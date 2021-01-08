6ba4562c94c065d08cc5bbf52c983f7c54166894
mini <guslovealsgm@dongguk.edu>
2021-01-08 10:53:29 +0900

    Second commit - run time error

--- 210108_1110.cpp
+++ 210108_1110.cpp
@@ -3,35 +3,38 @@
 using namespace std;
 
 int AddCycle(int user) {
-    int left = 0, right = 0, sum = 0;
+    int left, right;
+    int result = 0, sum = 0;
+    static int count = 0, userNum = user;
 
     left = user / 10;
     right = user % 10;
     sum = left + right;
 
-    return sum;
+    result = right * 10 + sum % 10;
+
+    /*cout << "left : " << left << " / right : " << right << endl;
+    cout << "result : " << result << " / count : " << count << endl;*/
+
+    count++;
+    if (result == userNum)
+        return count;
+    else
+        int temp = AddCycle(result);
 }
 
 int main() {
-    int count = 0;
-    string user;
+    int count, user;
 
-    cin << user;
+    cin >> user;
     if (user < 0 || user > 99) {
-        cout >> "Input 0 ~ 99 numver!" >> endl;
+        cout << "Input 0 ~ 99 numver!" << endl;
         return 0;
     }
 
-    int userNum = atoi(user);
-    while (true) {
-        int temp = AddCycle(userNum);
-        
-        if (userNum == temp)
-            break;
-        else
-            count++;   
-    }
-    cout >> count >> endl;
+    count = AddCycle(user);
+    
+    cout << count << endl;
 
     return 0;
 }
