00fbc888cde977950d75e24a1756c57d38a6931a
mini <guslovealsgm@dongguk.edu>
2021-01-11 10:49:27 +0900

    Second commit : wrong

--- 210111_1252.cpp
+++ 210111_1252.cpp
@@ -1,32 +1,50 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <deque>
 using namespace std;
+
+int bin(string user) {
+    int num = 0, index;
+
+    for (int i = 0; i < user.length(); i++) {
+        if (user[i] == '1') {
+            index = user.length() - 1 - i;
+            num += pow(2, index);
+        }
+    }
+
+    return num;
+}
 
 int main() {
     string user1, user2;
+    deque<int> result;
     int num1 = 0, num2 = 0, sum = 0;
 
-    /*cin >> num1 >> num2;*/
-    user1 = "1001101";
-    user2 = "10010";
+    cin >> user1 >> user2;
 
-    int index;
-    for (int i = 0; i < user1.length(); i++) {
-        if (user1[i] == '1') {
-            index = user1.length() - 1 - i;
-            num1 += pow(2, index);
-        }
-    }
-    for (int i = 0; i < user2.length(); i++) {
-        if (user2[i] == '1') {
-            index = user2.length() - 1 - i;
-            num2 += pow(2, index);
-        }
-    }
+    num1 = bin(user1);
+    num2 = bin(user2);
 
-    cout << num1 << endl;
-    cout << num2 << endl;
+    cout << "num1 : " << num1 << endl;
+    cout << "num2 : " << num2 << endl;
+
+    sum = num1 + num2;
+
+    cout << "sum : " << sum << endl;
+
+    cout << "result : ";
+    if (sum == 0)
+        cout << "0" << endl;
+    else {
+        for (int i = 0; sum != 0; i++) {
+            result.push_front(sum % 2);
+            sum /= 2;
+        }
+        for (int i = 0; i < result.size(); i++)
+            cout << result.at(i);
+    }
 
     return 0;
 }
