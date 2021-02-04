4883f22d8aa8021185d31368c570060e000cedab
mini <guslovealsgm@dongguk.edu>
2021-02-04 11:52:16 +0900

    Queue algorithm

--- 210203_10845.cpp
+++ 210203_10845.cpp
@@ -5,43 +5,68 @@
 using namespace std;
 
 class Queue {
-public:
-    int front, rear, maxSize;
+private:
+    int frontIdx, rearIdx;
+    int dataSize, maxSize;
     int* values;
 
+public:
     Queue(int size) {
+        frontIdx = 0;
+        rearIdx = 0;
+        dataSize = 0;
         maxSize = size;
         values = new int[size];
-        front = 0;
-        rear = 0;
     }
     ~Queue() {
         delete[] values;
     }
 
-    void Push(int value) {
-
+    void Push(int data) {
+        if (!IsFull()) {
+            values[rearIdx] = data;
+            rearIdx++;
+            dataSize++;
+        }
+        else
+            cout << "-1" << endl;
     }
     int Pop() {
+        int temp;
 
+        if (!IsEmpty()) {
+            temp = values[frontIdx];
+            frontIdx++;
+            dataSize--;
+
+            return temp;
+        }
+        else
+            return -1;
     }
     int GetFront() {
-
+        if (!IsEmpty())
+            return values[frontIdx];
+        else
+            return -1;
     }
     int GetBack() {
-
+        if (!IsEmpty())
+            return values[rearIdx - 1];
+        else
+            return -1;
     }
     int GetSize() {
-
+        return dataSize;
     }
     bool IsEmpty() {
-        if (front == rear)
+        if (dataSize == 0)
             return true;
         else
             return false;
     }
     bool IsFull() {
-        if ((rear + 1) % maxSize == front)
+        if (frontIdx + 1 + dataSize == maxSize)
             return true;
         else
             return false;
@@ -53,6 +78,40 @@ int main() {
 
     cin >> maxSize;
     Queue q(maxSize);
+
+    while (true) {
+        char user[10];
+
+        cin >> user;
+        if (strcmp(user, "push") == 0) {
+            int data;
+
+            cin >> data;
+            q.Push(data);
+        }
+        else if (strcmp(user, "pop") == 0) {
+            cout << q.Pop() << endl;
+        }
+        else if (strcmp(user, "size") == 0) {
+            cout << q.GetSize() << endl;
+        }
+        else if (strcmp(user, "empty") == 0) {
+            int empty = 0;
+
+            if (q.IsEmpty())
+                empty = 1;
+
+            cout << empty << endl;
+        }
+        else if (strcmp(user, "front") == 0) {
+            cout << q.GetFront() << endl;
+        }
+        else if (strcmp(user, "back") == 0) {
+            cout << q.GetBack() << endl;
+        }
+        else
+            break;
+    }
 
     return 0;
 }
